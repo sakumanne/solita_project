5a0be32a3d888486a93bcbfca891695d79218eb6
sakumanne <sakari.heinio@outlook.com>
2026-01-27 13:04:10 +0200

    cleaned up stuff and added additional ai models

--- audiomain.py
+++
@@ -1,9 +0,0 @@
-# Testi main whisperin testaamiseen erikseen
-from holoscantests.speech_rec.audio_app import AudioTranscriptionApp
-
-def main():
-    app = AudioTranscriptionApp()
-    app.run()
-
-if __name__ == "__main__":
-    main()
